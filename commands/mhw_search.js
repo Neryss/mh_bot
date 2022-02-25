72ebd33dc3bb74b4473d8815da6d6ff616d05fc8
neryss <neryssistos@gmail.com>
2022-02-25 22:54:16 +0100

    fixing command registration

---
+++ commands/mhw_search.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("mhw")
+		.setDescription("Search for a MHW monster stats")
+		.addStringOption(option =>
+			option.setName('name')
+				.setDescription('monster to search for')
+				.setRequired(true)),
+}
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
+const commands = [];
 require('dotenv').config();
 
-module.exports = client = new Client({intents: [Intents.FLAGS.GUILDS, 'GUILD_MESSAGES']});
+module.exports = client = new Client({ intents: [Intents.FLAGS.GUILDS, 'GUILD_MESSAGES'] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+console.log(commandFiles);
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
 	client.commands.set(command.data.name, command);
 }
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require("discord-api-types/v9");
 
 client.login(process.env.DISCORD_TOKEN);
 client.on('ready', () => {
 	console.log("Logged in");
+	const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+	(async () => {
+		try {
+			await rest.put(
+				// Routes.application
+				Routes.applicationGuildCommands(process.env.CLIENT_ID),
+				{ body: commands },
+			);
+			console.log("Commands registered");
+		} catch (err) {
+			console.log(err);
+		}
+	})();
 })
